036357a3776ad8c0022f53fd6d073cb5223270ff
kirisaki-vk <tolotrman.corp@gmail.com>
2023-05-04 14:14:35 +0300

    fix: import syntax and more general base

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-import { BrowserWindow } from "electron";
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+function createWindow(){
+    let win = new BrowserWindow({
+        width: 500,
+        height: 500,
+    });
 
+    win.loadFile(path.join(__dirname, "res/index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
